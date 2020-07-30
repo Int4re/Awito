57572de45894bd614e5e0710abad3d89c9a80b5e
Вячеслав <65826418+Int4re@users.noreply.github.com>
2020-07-30 19:27:51 +0500

    add Eslint

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // Массив для хранения карточек
 const dataBase = JSON.parse(localStorage.getItem('awito')) || [];
 
@@ -7,27 +5,27 @@ const dataBase = JSON.parse(localStorage.getItem('awito')) || [];
 let counter = dataBase.length;
 
 // Переменные с классами элементов из html
-const modalAdd = document.querySelector('.modal__add'),
-  addAd = document.querySelector('.add__ad'),
-  modalBtnSubmit = document.querySelector('.modal__btn-submit'),
-  modalSubmit = document.querySelector('.modal__submit'),
-  catalog = document.querySelector('.catalog'),
-  modalItem = document.querySelector('.modal__item'),
-  modalBtnWarning = document.querySelector('.modal__btn-warning'),
-  modalFileInput = document.querySelector('.modal__file-input'),
-  modalFileBtn = document.querySelector('.modal__file-btn'),
-  modalImageAdd = document.querySelector('.modal__image-add');
+const modalAdd = document.querySelector('.modal__add');
+const addAd = document.querySelector('.add__ad');
+const modalBtnSubmit = document.querySelector('.modal__btn-submit');
+const modalSubmit = document.querySelector('.modal__submit');
+const catalog = document.querySelector('.catalog');
+const modalItem = document.querySelector('.modal__item');
+const modalBtnWarning = document.querySelector('.modal__btn-warning');
+const modalFileInput = document.querySelector('.modal__file-input');
+const modalFileBtn = document.querySelector('.modal__file-btn');
+const modalImageAdd = document.querySelector('.modal__image-add');
 
 // Получение элементов для модального окна
-const modalImageItem = document.querySelector('.modal__image-item'),
-  modalHeaderItem = document.querySelector('.modal__header-item'),
-  modalStatusItem = document.querySelector('.modal__status-item'),
-  modalDescriptionItem = document.querySelector('.modal__description-item'),
-  modalCostItem = document.querySelector('.modal__cost-item');
+const modalImageItem = document.querySelector('.modal__image-item');
+const modalHeaderItem = document.querySelector('.modal__header-item');
+const modalStatusItem = document.querySelector('.modal__status-item');
+const modalDescriptionItem = document.querySelector('.modal__description-item');
+const modalCostItem = document.querySelector('.modal__cost-item');
 
 // Переменные для реализации поиска
-const searchInput = document.querySelector('.search__input'),
-  menuContainer = document.querySelector('.menu__container');
+const searchInput = document.querySelector('.search__input');
+const menuContainer = document.querySelector('.menu__container');
 
 // Временные переменные
 const textFileBtn = modalFileBtn.textContent;
@@ -35,7 +33,8 @@ const srcModalImage = modalImageAdd.src;
 
 // Получение всех элементов формы кроме кнопки
 const elementsModalSubmit = [...modalSubmit.elements].filter(
-  (elem) => elem.tagName !== 'BUTTON' && elem.type !== 'submit');
+  (elem) => elem.tagName !== 'BUTTON' && elem.type !== 'submit',
+);
 
 // Объект для хранения фото
 const infoPhoto = {};
@@ -52,24 +51,24 @@ const checkForm = () => {
 
 // Код для закрытия модальных окон
 const closeModal = (event) => {
-  const target = event.target;
-  if (target.closest('.modal__close') ||
-    target.classList.contains('modal') ||
-    event.code === 'Escape') {
-        modalAdd.classList.add('hide');
-        modalItem.classList.add('hide');
-        document.removeEventListener('keydown', closeModal);
-        modalSubmit.reset();
-        modalImageAdd.src = srcModalImage;
-        modalFileBtn.textContent = textFileBtn;
-        checkForm();
+  const { target } = event;
+  if (target.closest('.modal__close')
+    || target.classList.contains('modal')
+    || event.code === 'Escape') {
+    modalAdd.classList.add('hide');
+    modalItem.classList.add('hide');
+    document.removeEventListener('keydown', closeModal);
+    modalSubmit.reset();
+    modalImageAdd.src = srcModalImage;
+    modalFileBtn.textContent = textFileBtn;
+    checkForm();
   }
 };
 
 // Вывод карточек товара
 const renderCard = (DB = dataBase) => {
   catalog.textContent = '';
-  DB.forEach(item => {
+  DB.forEach((item) => {
     catalog.insertAdjacentHTML('beforeend', `
       <li class = "card" data-id-item="${item.counter}">
         <img class = "card__image" src = "data:image/jpeg;base64,${item.image}" alt = "test"/>
@@ -86,31 +85,29 @@ const renderCard = (DB = dataBase) => {
 searchInput.addEventListener('input', () => {
   const valueSearch = searchInput.value.trim().toLowerCase();
   if (valueSearch.length > 2) {
-    const result = dataBase.filter(item =>
-      item.nameItem.toLowerCase().includes(valueSearch) ||
-      item.descriptionItem.toLowerCase().includes(valueSearch)
-    );
+    const result = dataBase.filter((item) => item.nameItem.toLowerCase().includes(valueSearch)
+      || item.descriptionItem.toLowerCase().includes(valueSearch));
     renderCard(result);
   }
 });
 
 // Формирование фото
 modalFileInput.addEventListener('change', (event) => {
-  const target = event.target;
+  const { target } = event;
   const reader = new FileReader();
   const file = target.files[0];
   infoPhoto.filename = file.name;
   infoPhoto.size = file.size;
   reader.readAsBinaryString(file);
-  reader.addEventListener('load', (event) => {
+  reader.addEventListener('load', (e) => {
     if (infoPhoto.size < 200000) {
-        modalFileBtn.textContent = infoPhoto.filename;
-        infoPhoto.base64 = btoa(event.target.result);
-        modalImageAdd.src = `data:image/jpeg;base64,${infoPhoto.base64}`;
+      modalFileBtn.textContent = infoPhoto.filename;
+      infoPhoto.base64 = btoa(e.target.result);
+      modalImageAdd.src = `data:image/jpeg;base64,${infoPhoto.base64}`;
     } else {
-        modalFileBtn.textContent = 'Размер файла не должен превышать 200 кб';
-        modalFileInput.value = '';
-        checkForm();
+      modalFileBtn.textContent = 'Размер файла не должен превышать 200 кб';
+      modalFileInput.value = '';
+      checkForm();
     }
   });
 });
@@ -122,9 +119,11 @@ modalSubmit.addEventListener('input', checkForm);
 modalSubmit.addEventListener('submit', (event) => {
   event.preventDefault();
   const itemObj = {};
+  // eslint-disable-next-line no-restricted-syntax
   for (const elem of elementsModalSubmit) {
     itemObj[elem.name] = elem.value;
   }
+  // eslint-disable-next-line no-plusplus
   itemObj.counter = counter++;
   itemObj.image = infoPhoto.base64;
   dataBase.push(itemObj);
@@ -144,12 +143,12 @@ addAd.addEventListener('click', () => {
 
 // Открытие модального окна товара
 catalog.addEventListener('click', (event) => {
-  const target = event.target;
+  const { target } = event;
   const card = target.closest('.card');
 
   if (card) {
     console.log(card.dataset);
-    const item = dataBase.find(obj => obj.counter === +card.dataset.idItem);
+    const item = dataBase.find((obj) => obj.counter === +card.dataset.idItem);
     modalImageItem.src = `data:image/jpeg;base64,${item.image}`;
     modalHeaderItem.textContent = item.nameItem;
     modalStatusItem.textContent = item.status === 'new' ? 'Новый' : 'Б/У';
@@ -161,13 +160,13 @@ catalog.addEventListener('click', (event) => {
 });
 
 // Фильтрация по категориям
-menuContainer.addEventListener('click', event => {
-    const target = event.target;
+menuContainer.addEventListener('click', (event) => {
+  const { target } = event;
 
-    if (target.tagName === 'A') {
-      const result = dataBase.filter(item => item.category === target.dataset.category);
-      renderCard(result);
-    }
+  if (target.tagName === 'A') {
+    const result = dataBase.filter((item) => item.category === target.dataset.category);
+    renderCard(result);
+  }
 });
 
 // Закрытие модальных окон при нажатии за границей модального окна или на крестик
